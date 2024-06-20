ea22b37a20110f25391e88132199a8038332193e
karavan <karavan@test.org>
2024-06-20 12:05:43 +0000

    20/06/2024, 17:35:41

--- kamelets/DataTransform.java
+++ kamelets/DataTransform.java
@@ -2,13 +2,28 @@ import org.apache.camel.BindToRegistry;
 import org.apache.camel.Configuration;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import java.util.*;
+import java.io.File;
 
 @Configuration
 @BindToRegistry("DataTransform")
 public class DataTransform implements Processor {
 
     public void process(Exchange exchange) throws Exception {
-        String source = exchange.getProperty("admFilePath", String.class);
-        exchange.getIn().setBody(source);
+        //Map headers = exchange.getIn().getHeaders();
+        //Adm file path: {admFilePath=sample, CamelFileExchangeFile=RemoteFile[DPS_Sample.csv]}
+        String admFilePath = exchange.getProperty("admFilePath", String.class);
+        String body = exchange.getIn().getBody(String.class);
+        Map<String, Object> headers = exchange.getProperties();
+        System.out.println("Adm file path: " + admFilePath);
+
+        File file = new File(admFilePath);
+        Scanner sc = new Scanner(file);
+ 
+        while (sc.hasNextLine())
+            System.out.println(sc.nextLine());
+        
+        exchange.getIn().setBody("Hello from kamelet");
+        
     }
 }
